796531676ad08a0bba5ebdb2910392d327087367
sarva1210 <sarva.vish77@gmail.com>
2026-04-29 20:54:51 +0530

    google oauth

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "/auth/google/callback"
-}, (_, __, profile, done) => {
+}, 
+(_, __, profile, done) => {
     return done(null, profile);
 }))
 
